e2fea65b02c52c5170fe6db734575e1e9f7457bd
Yifan-Song <songyifanbj@163.com>
2019-08-01 21:03:42 +0800

    Update dataApi.py

--- dataApi.py
+++ dataApi.py
@@ -8,11 +8,14 @@ import requests
 import pandas as pd
 
 PATH = "http://data.natapp1.cc"
+nan="NAN"
 
 def getFuturesData(futures, start, end, freq, datatype, token, database='uqer'):
     '''Get Futures Data'''
     print("Getting Data...")
     payload = {'futures': futures, 'start' : start, 'end' : end, 'freq' : freq, 'datatype' : datatype, 'database':database, 'token' : token}
     res = requests.get(PATH+"/data", params=payload)
+    if("ERROR" in res.text):
+        return pd.DataFrame({"ERROR:" : res.text})
     df = pd.DataFrame(eval(res.text))
     return df
